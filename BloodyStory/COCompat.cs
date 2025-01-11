c231a11d8173a733cdd45cdb5b5712da7eaeb569
Professor Cupcake <profcupcake@hotmail.co.uk>
2025-01-11 17:28:10 +0000

    Combat Overhaul compat fix
    No longer breaks when CO is not loaded

---
+++ BloodyStory/COCompat.cs
@@ -0,0 +1,25 @@
+﻿using CombatOverhaul.DamageSystems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Server;
+
+namespace BloodyStory
+{
+    static class COCompat
+    {
+        public static void AddCODamageEH(IServerPlayer player, EntityBehaviorBleed bleedEB)
+        {
+            PlayerDamageModelBehavior pDamageModel = player.Entity.GetBehavior<PlayerDamageModelBehavior>();
+
+            pDamageModel.OnReceiveDamage += (ref float dmg, DamageSource dmgSrc, PlayerBodyPart bodyPart) =>
+            {
+                dmg = bleedEB.HandleDamage(player, dmg, dmgSrc);
+            };
+        }
+    };
+}
